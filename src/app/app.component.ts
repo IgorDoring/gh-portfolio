1c52ca87cde57ca2b657fb28aef0cb02962e72ce
IgorDoring <idoring@tutanota.com>
2024-04-11 01:02:41 -0300

    finishing with SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from './github.service';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'gh-portfolio';
   username = '';
 
-  constructor(private githubService: GithubService) { }
+  constructor(
+    private githubService: GithubService,
+    private titleService: Title,
+    private meta: Meta
+  ) {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.username = this.githubService.username;
+    this.titleService.setTitle('GitHub portfolio');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${this.username}'s GitHub portfolio with SSR`,
+      },
+      {
+        name: 'author',
+        content: this.username,
+      },
+    ]);
   }
 }
